8c227d14b5f1a299b01f5f6d1412e02a02b0a2f6
Eneojo22 <106937217+Eneojo22@users.noreply.github.com>
2022-07-06 02:26:18 +0100

    Update 100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -20,7 +20,7 @@ void print_times_table(int n)
 		_putchar('0');
 		_putchar(',');
 		_putchar(' ');
-		for (cone = 1; cone <= 2; cone++)
+		for (cone = 1; cone <= n; cone++)
 		{
 			d = (rone * cone);
 			if ((d / 10) > 0)
@@ -31,9 +31,10 @@ void print_times_table(int n)
 			{
 				_putchar(' ');
 			}
+                        _putchar(' ');
 			_putchar((d % 10) + '0');
-
-			if (cone < 9)
+   
+			if (cone < n)
 			{
 				_putchar(',');
 				_putchar(' ');
